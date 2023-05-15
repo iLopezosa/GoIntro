9ec0bca8d75bd69b90dd52ec0727da2a3cb68afa
Ignacio Lopezosa Serrano <n4xo@pm.me>
2023-05-15 21:10:00 +0200

    func

--- src/main.go
+++ src/main.go
@@ -2,26 +2,26 @@ package main
 
 import "fmt"
 
+func hi(msg string) {
+	fmt.Println(msg)
+}
+
+func threeArgs(a, b int, c string) {
+	fmt.Println(a, b, c)
+}
+
+func returnVal(a int) int {
+	return a * 2
+}
+
+func return2Vals(a int) (c, d int) {
+	return a, a * 2
+}
+
 func main() {
-	const hi = "Hello"
-	const world = "World!"
-
-	// Println()
-	fmt.Println(hi, world)
-	fmt.Println(hi, world)
-
-	// Printf()
-	platzi := "Platzi"
-	courses := 500
-	fmt.Printf("%s has more than %d courses\n", platzi, courses)
-	fmt.Printf("%v has more than %d courses\n", platzi, courses)
-
-	// Sprintf()
-	message := fmt.Sprintf("%s has more than %d courses", platzi, courses)
-	fmt.Println(message)
-
-	// Data types
-	fmt.Printf("message: %T\n", message)
-	fmt.Printf("courses: %T\n", courses)
-
+	hi("Hello World")
+	threeArgs(1, 2, "hi")
+	fmt.Println("Value:", returnVal(4))
+	val1, val2 := return2Vals(3)
+	fmt.Println("Values:", val1, val2)
 }
