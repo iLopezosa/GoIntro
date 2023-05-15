8618377f4ffdf1161f3b320ff79380c5e1211434
Ignacio Lopezosa Serrano <n4xo@pm.me>
2023-05-15 21:24:55 +0200

    for

--- src/main.go
+++ src/main.go
@@ -2,26 +2,26 @@ package main
 
 import "fmt"
 
-func hi(msg string) {
-	fmt.Println(msg)
-}
-
-func threeArgs(a, b int, c string) {
-	fmt.Println(a, b, c)
-}
-
-func returnVal(a int) int {
-	return a * 2
-}
-
-func return2Vals(a int) (c, d int) {
-	return a, a * 2
-}
-
 func main() {
-	hi("Hello World")
-	threeArgs(1, 2, "hi")
-	fmt.Println("Value:", returnVal(4))
-	val1, val2 := return2Vals(3)
-	fmt.Println("Values:", val1, val2)
+
+	// Conditional For
+	for i := 0; i <= 10; i++ {
+		fmt.Print(i, ", ")
+	}
+
+	fmt.Println()
+
+	// For while
+	counter := 0
+	for counter <= 10 {
+		fmt.Print(counter, ", ")
+		counter++
+	}
+
+	// For forever
+	// foreverCounter := 0
+	// for {
+	// 	fmt.Print(foreverCounter, ", ")
+	// 	foreverCounter++
+	// }
 }
