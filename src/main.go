aef97592066cd2219ecd388b5514d6b81807d864
Ignacio Lopezosa Serrano <n4xo@pm.me>
2023-05-15 20:41:38 +0200

    Vars, consts and zeros

--- src/main.go
+++ src/main.go
@@ -3,5 +3,25 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Constants declaration
+	const pi float64 = 3.14159265359
+	const pi2 = 3.1416
+
+	fmt.Println("pi:", pi)
+	fmt.Println("pi2:", pi2)
+
+	// Variables declaration
+	base := 12          // infers the type of variable
+	var height int = 13 // explicit type declaration
+	var area int        // zero value
+
+	fmt.Println(base, height, area)
+
+	// Zero values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
 }
