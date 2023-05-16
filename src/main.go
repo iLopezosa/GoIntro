8ebc553a12b1810ca559a59882f3fb643543c735
Ignacio Lopezosa Serrano <n4xo@pm.me>
2023-05-16 21:34:44 +0200

    Why is Slow faster?

--- src/main.go
+++ src/main.go
@@ -2,31 +2,49 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
+func isPalyndromeFast(text string) bool {
+	var length int = len(text) - 1
+
+	for i := range text {
+		if text[i] != text[length-i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func isPalyndromeSlow(text string) bool {
+	var reverseText string
+
+	for i := len(text) - 1; i >= 0; i-- {
+		reverseText += string(text[i])
+	}
+
+	return reverseText == text
+}
+
 func main() {
-	// Array
-	var array [4]int
-	array[0] = 1
-	array[1] = 2
-	fmt.Println(array, len(array), cap(array))
+	// slice := []string{"hi", "what's", "up"}
 
-	// Slice
-	slice := []int{1, 2, 3, 4, 5, 6}
-	fmt.Println(slice, len(slice), cap(slice))
-
-	// Slice methods
-	fmt.Println(slice[0])
-	fmt.Println(slice[:3])
-	fmt.Println(slice[2:4])
-	fmt.Println(slice[4:])
-
-	// Append
-	slice = append(slice, 7)
-	fmt.Println(slice)
-
-	// Append new list
-	newSlice := []int{8, 9, 10}
-	slice = append(slice, newSlice...)
-	fmt.Println(slice)
+	// for i, value := range slice {
+	// 	fmt.Printf("The characters of the %d:%s string are of type %T\n", i, value, slice[i][0])
+	// }
+	start := time.Now()
+	fmt.Println(isPalyndromeFast("amor a roma"))
+	fmt.Println(isPalyndromeFast("ama"))
+	fmt.Println(isPalyndromeFast("ieanorstienariostenaorisetnaiorsetnaioe"))
+	fmt.Println(isPalyndromeFast("aiorsetnarioarsarstarstoaiersntoiearnsoitenarsioteiarnsoeitniarensotieanrsioetnaioresntoiearnsiotenariosetnaioresntioaernsiotenariosetnarioesntioarenstioeanrsiotenarioestnoiaresntioarenstoiaernstioearnsiotenarsioetnaorisetnioaresntioearnsoitenarsiotenaroisetnoiaresntoiaernstioeanrssetnaoiresntoaiersntoiaernstoiearnsoitenarsioetnariosetnioaresntioearnsiotenarsiotenariosetnarsktgienwkfein"))
+	fmt.Println(isPalyndromeFast("arst arst arst tsra tsra tsra"))
+	fmt.Printf("Fast time: %d\n", time.Since(start))
+	start = time.Now()
+	fmt.Println(isPalyndromeSlow("amor a roma"))
+	fmt.Println(isPalyndromeSlow("ama"))
+	fmt.Println(isPalyndromeSlow("ieanorstienariostenaorisetnaiorsetnaioe"))
+	fmt.Println(isPalyndromeFast("aiorsetnarioarsarstarstoaiersntoiearnsoitenarsioteiarnsoeitniarensotieanrsioetnaioresntoiearnsiotenariosetnaioresntioaernsiotenariosetnarioesntioarenstioeanrsiotenarioestnoiaresntioarenstoiaernstioearnsiotenarsioetnaorisetnioaresntioearnsoitenarsiotenaroisetnoiaresntoiaernstioeanrssetnaoiresntoaiersntoiaernstoiearnsoitenarsioetnariosetnioaresntioearnsiotenarsiotenariosetnarsktgienwkfein"))
+	fmt.Println(isPalyndromeSlow("arst arst arst tsra tsra tsra"))
+	fmt.Printf("Slow time: %d\n", time.Since(start))
 }
