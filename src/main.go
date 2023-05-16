799080b43ea2a0e2efa56c9e4dadce01a4b2e1a1
Ignacio Lopezosa Serrano <n4xo@pm.me>
2023-05-16 21:44:19 +0200

    Maps

--- src/main.go
+++ src/main.go
@@ -2,49 +2,23 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func isPalyndromeFast(text string) bool {
-	var length int = len(text) - 1
-
-	for i := range text {
-		if text[i] != text[length-i] {
-			return false
-		}
-	}
-
-	return true
-}
-
-func isPalyndromeSlow(text string) bool {
-	var reverseText string
-
-	for i := len(text) - 1; i >= 0; i-- {
-		reverseText += string(text[i])
-	}
-
-	return reverseText == text
-}
-
 func main() {
-	// slice := []string{"hi", "what's", "up"}
+	m := make(map[string]uint8)
 
-	// for i, value := range slice {
-	// 	fmt.Printf("The characters of the %d:%s string are of type %T\n", i, value, slice[i][0])
-	// }
-	start := time.Now()
-	fmt.Println(isPalyndromeFast("amor a roma"))
-	fmt.Println(isPalyndromeFast("ama"))
-	fmt.Println(isPalyndromeFast("ieanorstienariostenaorisetnaiorsetnaioe"))
-	fmt.Println(isPalyndromeFast("aiorsetnarioarsarstarstoaiersntoiearnsoitenarsioteiarnsoeitniarensotieanrsioetnaioresntoiearnsiotenariosetnaioresntioaernsiotenariosetnarioesntioarenstioeanrsiotenarioestnoiaresntioarenstoiaernstioearnsiotenarsioetnaorisetnioaresntioearnsoitenarsiotenaroisetnoiaresntoiaernstioeanrssetnaoiresntoaiersntoiaernstoiearnsoitenarsioetnariosetnioaresntioearnsiotenarsiotenariosetnarsktgienwkfein"))
-	fmt.Println(isPalyndromeFast("arst arst arst tsra tsra tsra"))
-	fmt.Printf("Fast time: %d\n", time.Since(start))
-	start = time.Now()
-	fmt.Println(isPalyndromeSlow("amor a roma"))
-	fmt.Println(isPalyndromeSlow("ama"))
-	fmt.Println(isPalyndromeSlow("ieanorstienariostenaorisetnaiorsetnaioe"))
-	fmt.Println(isPalyndromeFast("aiorsetnarioarsarstarstoaiersntoiearnsoitenarsioteiarnsoeitniarensotieanrsioetnaioresntoiearnsiotenariosetnaioresntioaernsiotenariosetnarioesntioarenstioeanrsiotenarioestnoiaresntioarenstoiaernstioearnsiotenarsioetnaorisetnioaresntioearnsoitenarsiotenaroisetnoiaresntoiaernstioeanrssetnaoiresntoaiersntoiaernstoiearnsoitenarsioetnariosetnioaresntioearnsiotenarsiotenariosetnarsktgienwkfein"))
-	fmt.Println(isPalyndromeSlow("arst arst arst tsra tsra tsra"))
-	fmt.Printf("Slow time: %d\n", time.Since(start))
+	m["Joseph"] = 0
+	m["Francis"] = 20
+	m["Anne"] = 32
+
+	fmt.Println(m)
+
+	// Read map
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+
+	// Get one value
+	value, found := m["Joseph"]
+	fmt.Println(value, found)
 }
