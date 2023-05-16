2643c665f5e19d9c7139d4c7c245106918e40318
Ignacio Lopezosa Serrano <n4xo@pm.me>
2023-05-16 21:02:40 +0200

    slices

--- src/main.go
+++ src/main.go
@@ -2,33 +2,31 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func longFunction() {
-	time.Sleep(4 * time.Second)
-	fmt.Println("Hi")
-}
-
 func main() {
-	// Defer
-	defer longFunction()
-	time.Sleep(2 * time.Second)
-	fmt.Println("world")
+	// Array
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
 
-	// Continue and break
-	for i := 0; i < 10; i++ {
-		// Continue
-		if i == 2 {
-			fmt.Println("It's 2")
-			continue
-		}
+	// Slice
+	slice := []int{1, 2, 3, 4, 5, 6}
+	fmt.Println(slice, len(slice), cap(slice))
 
-		if i == 8 {
-			fmt.Println("BREAK")
-			break
-		}
+	// Slice methods
+	fmt.Println(slice[0])
+	fmt.Println(slice[:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
 
-		fmt.Println(i)
-	}
+	// Append
+	slice = append(slice, 7)
+	fmt.Println(slice)
+
+	// Append new list
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
 }
