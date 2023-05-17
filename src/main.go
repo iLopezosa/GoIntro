56fa0b17695cceee9a2e50b3a5b1bae21e9545e3
Ignacio Lopezosa Serrano <n4xo@pm.me>
2023-05-17 23:00:18 +0200

    channels range, close, and select

--- src/main.go
+++ src/main.go
@@ -2,27 +2,51 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 )
 
-func say(txt string, c chan<- string) {
-	init := time.Now()
-	time.Sleep(time.Second * time.Duration(rand.Float64()*5))
-	txt = fmt.Sprintf("%s %s", txt, time.Since(init))
+func message(txt string, c chan string) {
 	c <- txt
 }
 
 func main() {
-	c := make(chan string, 3)
+	c := make(chan string, 2)
+	c <- "Msg 1"
+	c <- "Msg 2"
 
-	fmt.Println("Hi")
+	fmt.Println(len(c), cap(c))
 
-	go say("bye1", c)
-	go say("bye2", c)
-	go say("bye3", c)
+	// Range & close
+	close(c)
 
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
+	// c <- "Msg 3"
+
+	for msg := range c {
+		fmt.Println(msg)
+	}
+
+	//Select
+	email1 := make(chan string)
+	email2 := make(chan string)
+
+	go message("msg1", email1)
+	go message("msg2", email2)
+
+	for i := 0; i < 2; i++ {
+		select {
+		case m1 := <-email1:
+			fmt.Println("From email1:", m1)
+		case m2 := <-email2:
+			fmt.Println("From email2:", m2)
+		}
+	}
+
+	// fmt.Println("Hi")
+
+	// go say("bye1", c)
+	// go say("bye2", c)
+	// go say("bye3", c)
+
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
+	// fmt.Println(<-c)
 }
