1cdcf6236e21983ba3912fdb95934744d5831e13
Ignacio Lopezosa Serrano <n4xo@pm.me>
2023-05-17 21:20:18 +0200

    pointers and structs

--- src/main.go
+++ src/main.go
@@ -1,18 +1,37 @@
 package main
 
 import (
-	"net/http"
-
-	"github.com/labstack/echo"
+	"fmt"
 )
 
-func main() {
-	// Instantiate
-	e := echo.New()
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
 
-	// Route
-	e.GET("/", func(c echo.Context) error {
-		return c.String(http.StatusOK, "Hello there!")
-	})
-	e.Logger.Fatal(e.Start(":1323"))
+func (myPC pc) ping() {
+	fmt.Println(myPC.brand, "Pong")
+}
+
+func (myPC *pc) duplidateRAM() {
+	myPC.ram *= 2
+}
+
+func main() {
+	a := 50
+	b := &a
+	c := b
+
+	fmt.Println(a, &a, b, *b, *c)
+
+	*b = 100
+	fmt.Println(a, &a, b, *b, *c)
+
+	myPC := pc{ram: 8, disk: 512, brand: "asus"}
+	fmt.Println(myPC)
+
+	myPC.ping()
+	myPC.duplidateRAM()
+	fmt.Println(myPC)
 }
